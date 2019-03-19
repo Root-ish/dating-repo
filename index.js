42324b204523fd7e06a6af04fa7e8fe5da05410d
Root-ish <rickmichiel@icloud.com>
2019-03-19 13:43:04 +0100

    Add express.js and pug.js, add routing options.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-// import _ from 'lodash';
-// import _ from '../node_modules/lodash';
+var express = require('express')
+var lego = require('./routes/lego.js')
+
+express()
+    .use('/static', express.static('static'))
+    .use('/views', express.static('views'))
+    .use('/lego', lego)
+    .set('view engine', 'pug')
+    .get('/', function (req, res) {
+      res.render('index', {title: 'Pickle Rick', message: 'Wabba dabba dub dub'})
+    })
+    .get('*', notFound)
+    .listen(8000);
+
+    function notFound(req, res){
+      res.send("404 not found")
+    }
