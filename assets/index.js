3421e9596198258dd6589958da502d31ee0efaf6
Root-ish <rickmichiel@icloud.com>
2019-03-24 20:35:08 +0100

    Add fetch for beer api. Add search function

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+    //
+    // function beerSearch() {
+    //   fetch('http://example.com/movies.json')
+    //   .then(function(response) {
+    //     return response.json();
+    //   })
+    //   .then(function(myJson) {
+    //     console.log(JSON.stringify(myJson));
+    //   });
+    // }
--- server/routes.js
+++ server/routes.js
@@ -1,47 +1,100 @@
 const express = require('express')
-const users = require('./users')
+const bodyParser = require('body-parser')
 const beerlist = require('./beers')
 const find = require('array-find')
 const router = express.Router()
 const slug = require('slug')
 const multer = require('multer')
 const upload = multer({dest: 'assets/upload/'})
+const mongo = require('mongodb')
+let searchResults = [];
 
-router.get('/', (req, res) => {
-	res.render('home.ejs', {user: users})
-});
+require('dotenv').config()
 
-router.get('/:userId', (req, res, next) => {
-  var userId = req.params.userId
-  var user = find(users, function (value) {
-    return value.userId === userId
-  })
+var db = null
+var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
 
-  if (!user) {
-    next()
-    return
+mongo.MongoClient.connect(url, {useNewUrlParser: true}, function (err, client) {
+  if (err) throw err
+  db = client.db(process.env.DB_NAME)
+})
+
+// get list with users
+
+router.get('/', (req, res, next) => {
+	db.collection('users').find().toArray(done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.render('home.ejs', {user: data, beers: beerlist})
+    }
   }
 
-  res.render('user.ejs', {user: user, beers: beerlist})
 });
 
-router.get('/sign-up', (req, res) => {
-  res.render('sign-up.ejs', {beers: beerlist})
-});
+// post new user details
 
-router.post('/', upload.single('profilePhoto'), (req, res) => {
-  var userId = slug(req.body.name, {lower: true})
-
-  users.push({
-    userId: userId,
+router.post('/', (req, res, next) => {
+	console.log(req.body.name);
+	db.collection('users').insertOne({
     name: req.body.name,
     profileAbout: req.body.profileAbout,
     profilePhoto: req.file ? req.file.filename : null,
     description: req.body.description,
     beerProfile: req.body.beerProfile
-  })
+  }, done)
 
-  res.redirect('/' + userId)
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/' + data.insertedId)
+    }
+  }
+});
+
+// Get form for new user details
+
+router.get('/sign-up', (req, res) => {
+  res.render('sign-up.ejs',  {searchResults})
+});
+
+// Delete user
+
+router.delete('/:id', (req, res, next) => {
+	var id = req.params.id
+
+	db.collection('users').deleteOne({
+    _id: new mongo.ObjectID(id)
+  }, done)
+
+  function done(err) {
+    if (err) {
+      next(err)
+    } else {
+      res.json({status: 'ok'})
+    }
+  }
+});
+
+// get user details
+
+router.get('/:id', (req, res, next) => {
+  var id = req.params.id
+
+	db.collection('users').findOne({
+	 _id: mongo.ObjectID(id)
+ }, done)
+
+ function done(err, data) {
+	 if (err) {
+		 next(err)
+	 } else {
+		 res.render('user.ejs', {user: data, beers: beerlist})
+	 }
+ }
 });
 
 module.exports = router;
