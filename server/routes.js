bb39c85e9dc5507c252009fd1af660cf1a842646
Root-ish <rickmichiel@icloud.com>
2019-03-26 14:27:36 +0100

    Add sessions

--- server/routes.js
+++ server/routes.js
@@ -7,37 +7,128 @@ const slug = require('slug')
 const multer = require('multer')
 const upload = multer({dest: 'assets/upload/'})
 const mongo = require('mongodb')
+const session = require('express-session')
 let searchResults = [];
 
 require('dotenv').config()
 
 var db = null
 var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
+var user = process.env.DB_USER
 
 mongo.MongoClient.connect(url, {useNewUrlParser: true}, function (err, client) {
   if (err) throw err
   db = client.db(process.env.DB_NAME)
 })
 
-// get list with users
+router.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET
+}))
 
-router.get('/', (req, res, next) => {
-	db.collection('users').find().toArray(done)
+
+
+// Sign-up form
+
+router.get('/sign-up', (req, res) => {
+  res.render('sign-up.ejs')
+});
+
+// Post sign-up form
+
+router.post('/sign-up', (req, res, next) => {
+var username = req.body.username
+var password = req.body.password
+
+if (!username || !password) {
+  return res.status(400).send('Username or password are missing')
+}
+
+if (db.collection('users').find({name: username}) === username) {
+  return res.status(400).send('Username already exist')
+}
+
+else {
+  db.collection('users').insertOne({
+    name: username,
+    password: password
+}, done)
 
   function done(err, data) {
     if (err) {
       next(err)
     } else {
-      res.render('home.ejs', {user: data, beers: beerlist})
+      req.session.user = {username: username};
+      res.redirect('/')
     }
   }
-
+  }
 });
 
-// post new user details
+
+// Log-in form
+
+router.get('/log-in', (req, res) => {
+  res.render('log-in.ejs')
+});
+
+// Log-in Post
+
+router.post('/log-in', (req, res) => {
+var username = req.body.username
+var password = req.body.password
+
+db.collection('users').find({name: username}, done);
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      var user = db.collection('users').find({name: username})
+      req.session.user = {username: user.username};
+      res.redirect('/')
+    }
+  }
+});
+
+
+
+// Log-out
+
+router.get('/log-out', (req, res, next) => {
+    req.session.destroy(function (err) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/')
+    }
+  })
+});
+
+
+
+
+// Get form for adding new beer
+
+router.get('/addBeer', (req, res) => {
+  if (req.session.user) {
+    res.render('addBeer.ejs',  {searchResults})
+  } else {
+    return res.status(401).send('Not loged in...')
+  }
+});
+
+
+
+// Post for adding new beer
 
 router.post('/', (req, res, next) => {
-	console.log(req.body.name);
+
+  if (req.session.user) {
+
+  }
+
 	db.collection('users').insertOne({
     name: req.body.name,
     profileAbout: req.body.profileAbout,
@@ -55,13 +146,29 @@ router.post('/', (req, res, next) => {
   }
 });
 
-// Get form for new user details
 
-router.get('/sign-up', (req, res) => {
-  res.render('sign-up.ejs',  {searchResults})
+
+
+
+// get list of beers
+
+router.get('/', (req, res, next) => {
+	db.collection('users').find().toArray(done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.render('home.ejs', {user: data, beers: beerlist})
+    }
+  }
+
 });
 
-// Delete user
+
+
+
+// Delete beer
 
 router.delete('/:id', (req, res, next) => {
 	var id = req.params.id
@@ -79,7 +186,7 @@ router.delete('/:id', (req, res, next) => {
   }
 });
 
-// get user details
+// get beer details
 
 router.get('/:id', (req, res, next) => {
   var id = req.params.id
